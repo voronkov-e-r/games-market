162ef3c731ef262253493925a8ccf33688265fe0
voronkov-e-r <dox10750@gmail.com>
2026-08-02 20:30:01 +0300

    Completed a front view

--- front/market/urls.py
+++ front/market/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='market'),
+    path('library', views.library, name='library'),
+    path('topup', views.topup, name='tupup')
 ]
--- front/market/views.py
+++ front/market/views.py
@@ -3,6 +3,12 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'main_market.html', {'games':[Game('Team workers 2', 800) for i in range(10)], 'user':User('Анатолий',1286.31)})
 
+def library(request):
+    return render(request, 'library.html', {'user':User('Анатолий', 1286.31), 'library_games':[Game('Team workers 2', 800) for i in range(5)]})
+
+def topup(request):
+    return render(request, 'topup.html', {'user':User('Анатолий', 1286.31)})
+
 # Пока что имитация получения каких-то данных
 class Game:
     def __init__(self, name:str, price:int):
